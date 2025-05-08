f73a9e82546e3ccb511f373c77a3e02146941b21
JesusSuarezV <jesusalexandersv@ufps.edu.co>
2025-05-08 18:18:33 -0500

    Se sube el codigo

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
 COPY app/ .
--- app/main.py
+++ app/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI, Request
 import json
 import os
+import psycopg2
+from pydantic import BaseModel
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+def get_connection():
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
 
 app = FastAPI()
 
+class Note(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 async def root():
@@ -12,15 +32,35 @@ async def root():
         "You can use this API to manage your notes."
     }
 
-
 @app.get("/notes")
 async def get_notes():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, title, content FROM notes")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
 
-    # TODO: Implementar
-    return {"notes": []}
+    notes = []
+    for row in rows:
+        notes.append({
+            "id": row[0],
+            "title": row[1],
+            "content": row[2]
+        })
+
+    return {"notes": notes}
 
 
 @app.post("/notes")
-async def create_note(request: Request):
-    # TODO: Implementar
+async def create_note(note: Note):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO notes (title, content) VALUES (%s, %s)",
+    (note.title, note.content)
+    )
+    conn.commit()  
+    print("Note save in DB")
+    cur.close()
+    conn.close()
     return {"message": "Note created successfully!"}
